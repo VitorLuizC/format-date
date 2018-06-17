e2e9c39fa81f5e8c1edaccdd3018136cbf28d523
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-16 21:04:43 -0300

    Create tests

---
+++ test.js
@@ -0,0 +1,30 @@
+import test from 'ava';
+import nanodate from './';
+
+test('module API: Exports nanodate function as default', (context) => {
+  context.is(typeof nanodate, 'function');
+});
+
+test('Formatters: Characters return part of a DateTime', (context) => {
+  const date = new Date('0999-04-03 05:38:02');
+
+  context.is(nanodate(date, 'DD'), '03');
+  context.is(nanodate(date, 'D'), '3');
+  context.is(nanodate(date, 'MM'), '04');
+  context.is(nanodate(date, 'M'), '4');
+  context.is(nanodate(date, 'YYYY'), '0999')
+  context.is(nanodate(date, 'YY'), '99');
+  context.is(nanodate(date, 'hh'), '05');
+  context.is(nanodate(date, 'h'), '5');
+  context.is(nanodate(date, 'mm'), '38');
+  context.is(nanodate(date, 'm'), '38');
+  context.is(nanodate(date, 'ss'), '02');
+  context.is(nanodate(date, 's'), '2');
+});
+
+test('Formatters: Characters return part of a DateTime', (context) => {
+  const date = new Date('0999-04-03 05:38:02');
+  const format = 'DD/MM/YYYY [às] hh[h]mm[min] e s [segundos].';
+
+  context.is(nanodate(date, format), '03/04/0999 às 05h38min e 2 segundos.');
+});
