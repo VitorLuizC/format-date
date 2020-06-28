8deb0773cc132438d0f36787bff1791a81afb806
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-06-28 03:05:48 -0300

    ✨ improve JSDocs and cleanup the code

--- src/format-date.ts
+++ src/format-date.ts
@@ -1,19 +1,63 @@
-import formatters from './formatters';
-import matcher from './matcher';
+/**
+ * A dictionary that has tokens and their corresponding formatter function.
+ */
+// prettier-ignore
+const FORMATTERS: Record<string, (date: Date) => string> = {
+  'DD':   date => addZeroPads(2, '' + date.getDate()),
+  'D':    date => '' + date.getDate(),
+  'MM':   date => addZeroPads(2, '' + (date.getMonth() + 1)),
+  'M':    date => '' + (date.getMonth() + 1),
+  'YYYY': date => addZeroPads(4, '' + date.getFullYear()),
+  'YY':   date => ('' + date.getFullYear()).substr(-2),
+  'HH':   date => addZeroPads(2, '' + date.getHours()),
+  'H':    date => '' + date.getHours(),
+  'mm':   date => addZeroPads(2, '' + date.getMinutes()),
+  'm':    date => '' + date.getMinutes(),
+  'ss':   date => addZeroPads(2, '' + date.getSeconds()),
+  's':    date => '' + date.getSeconds(),
+};
 
 /**
- * It replaces format tokens for corresponding Date formats.
- * @example ```js
- * format(new Date(), 'DD/MM/YYYY hh:mm:ss')
- * ```
- * @param date A Date instace.
- * @param format A string with tokens based on moment.
+ * Part of the matcher, a `RegExp`, for escaping texts.
  */
-function format(date: Date, format: string) {
-  return format.replace(matcher, (token: string) => {
-    if (formatters.hasOwnProperty(token)) return formatters[token](date);
+const ESCAPE = '\\[[^\\[\\]]*\\]';
+
+/**
+ * Creates the matcher using formatters' tokens and the escaping strategy.
+ * @returns {RegExp}
+ */
+function createMatcher(): RegExp {
+  const matchers = Object.keys(FORMATTERS).concat(ESCAPE);
+  return new RegExp(matchers.join('|'), 'g');
+}
+
+/**
+ * Add zero (`0`) pads to text's length match defined length.
+ * @param {Number} length - Expected length of text with zero pads.
+ * @param {String} text - Text that receives the zero pads if below length.
+ * @returns {String}
+ */
+function addZeroPads(length: number, text: string): string {
+  if (text.length >= length) return text;
+  return addZeroPads(length, '0' + text);
+}
+
+/**
+ * Receives a `Date` and a format (with tokens based on Moment.js) in `string`
+ * and returns the same format replacing the tokens for values from `Date`.
+ * @example
+ * formatDate(new Date(), "DD/MM/YYYY hh:mm:ss");
+ * //=> "25/06/2020 11:59:28"
+ *
+ * formatDate(new Date(), "[Day] D [at] h'mm");
+ * //=> "Day 25 at 11'59"
+ * @param {Date} date - A `Date` instance.
+ * @param {String} format - A string with tokens (like Moment.js tokens).
+ * @returns {String}
+ */
+export default function formatDate(date: Date, format: string): string {
+  return format.replace(createMatcher(), (token: string) => {
+    if (FORMATTERS.hasOwnProperty(token)) return FORMATTERS[token](date);
     return token.replace(/\[|\]/g, '');
   });
 }
-
-export default format;
